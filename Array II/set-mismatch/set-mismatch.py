e581ff227d88ff15dc07d11229234605311468f7
PATHAN MOHAMMAD SAADH <pmdsaadh723@gmail.com>
2026-07-26 22:19:33 +0530

    Update set-mismatch.py

--- Array II/set-mismatch/set-mismatch.py
+++ Array II/set-mismatch/set-mismatch.py
@@ -1,1 +1,18 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        count = [0] * (n + 1)
 
+        for num in nums:
+            count[num] += 1
+
+        duplicate = 0
+        misssing = 0
+
+        for num in range(1, n + 1):
+            if count[num] == 2:
+                duplicate = num
+            elif count[num] == 0:
+                missing = num
+
+        return [duplicate, missing]
